b15b70ba2380e478de825db131d856cdd3ea90a8
Alan Jones <jones.alan21@gmail.com>
2018-05-02 10:01:45 -0400

    added images, CSS

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,19 @@ const captions = {
     toughGuy: ["Worrying about your followers, you need to get your dollars up.", "I'm up right now and you suck right now.", "Pray the real live forever man, pray the fakes get exposed.", "Know yourself, know your worth.", "I don’t take naps. Me and the money are way too attached to go and do that.", "I’m in it for the glory, not the honour mention.", "Hermès link, ice-blue mink.", "She say, Do you love me?, I tell her only partly, I only love my bed and my mama, I'm sorry!", "I wear every single chain, even when I’m in the house", "Overdosed on confidence.", "Last name ever. First name greatest.", "Trigger fingers turn to twitter fingers.", "What a time to be alive.", "Pray the real live forever, man. Pray the fakes get exposed.", "I order that Alfredo pasta, Then eat in the kitchen like I’m in the mafia" ],
     ladiesMan: ["You the best I ever had", "Sweatpants, hair tied, chillin' with no makeup on.", "I’m blem for real, I might just say how I feel.", "We need to forward to the islands and get you gold, no spray tans.", "Out of body. That’s just how I feel when I’m around you, shawty.", "I know shorty and she doesn't want no slow song.", "You ain't stressin' off no lover in the past tense, You already had them.", "'Cause you're a good girl and you know it.", "Accept yourself, you don't have to prove s*** to no one except yourself.", "I want your hot love and emotion endlessly."],
     broskis: ["I can't really see another squad tryna cross us.", "Nobody really likes us except for us.", "My team good, we don't really need a mascot.", "Started from the bottom now the whole team here.", "I'm with my whole set, tennis matches at the crib, I swear I could beat Serena when she playing with her left.", "No new friends, we don't feel that.", "The haters just bringing me and my people closer.", "Imagine if I never met the broskis.", "Used to work the fries, now we supersize.", "My whole team winning, we don't play your game."],
-    goodTimes: ["I live for the nights that I can't remember with the people that I won't forget.", "World Series attitude, champagne bottle life.", "Somewhere between I'm sober and I'm lifted.", "Drinking every night because we drink to my accomplishments.", "Twenty-four-hour champagne diet.", "Ask you to please excuse my table manners, I was making room for the table dancers.", "Your boy all glowed up. Every time we touch down bet it go up.", "Taking shots, feeling bulletproof.", "Turn my birthday into a lifestyle.", "You only live once, that's the motto.", "YOLO", "Young, but I'm makin millions to work the night shift.", "I like it when money makes a difference but don't make you different.", "I just think its funny how it goes, Now I’m on the road, half a million for a show"],
+    goodTimes: ["I live for the nights that I can't remember with the people that I won't forget.", "World Series attitude, champagne bottle life.", "Somewhere between I'm sober and I'm lifted.", "Drinking every night because we drink to my accomplishments.", "Twenty-four-hour champagne diet.", "Ask you to please excuse my table manners, I was making room for the table dancers.", "Your boy all glowed up. Every time we touch down bet it go up.", "Taking shots, feeling bulletproof.", "Turn my birthday into a lifestyle.", "You only live once, that's the motto.", "YOLO", "Young, but I'm makin millions to work the night shift.", "I like it when money makes a difference but don't make you different.", "I just think its funny how it goes, Now I’m on the road, half a million for a show", "I want this forever / I swear I could spend whatever on it."],
     sixGod: ["Runnin' through the 6 with my woes.", "How did I finesse all of this from Jane and Weston?", "I got real ones livin’ past Kennedy Road, I got real ones with me everywhere that I go", "Been flowin’ stupid since Vince Carter was on some through the legs, arm in the hoop s***.", "This ain't the son you raised who used to take the Acura, 5 AM going shoot Degrassi up on Morningside", "I take Eglinton to 401 East and exit at Markham Road in the east end, Where all the pretty girls are sleeping.", "Queen Street visions that nobody believed in.", "We evolved, used to think vacation meant Niagara Falls.", "Yeah, used to be at SilverCity, Indigo, Used to be in lunchroom, playin’ dominoes.", "I'm buildin' up a house where they raised me", "The 6 ain't friendly but it's where I lay up"]
 };
+
+//Possible images
+
+const images = {
+    sadBoi: ["images/sadBoi1.jpg", "images/sadBoi2.jpg", "images/sadBoi3.jpg", "images/sadBoi4.jpg", "images/sadBoi5.jpg" ],
+    toughGuy: ["images/toughGuy1.jpg", "images/toughGuy2.jpg", "images/toughGuy3.jpg", "images/toughGuy4.jpg", "images/toughGuy5.jpg"],
+    broskis: ["images/broskis1.jpg", "images/broskis2.jpg", "images/broskis3.jpg", "images/broskis4.jpg", "images/broskis5.jpg" ],
+    goodTimes: ["images/goodTimes1.jpg", "images/goodTimes2.jpg", "images/goodTimes3.jpg", "images/goodTimes4.jpg", "images/goodTimes5.jpg"],
+    sixGod: ["images/sixGod1.jpg", "images/sixGod2.jpg", "images/sixGod3.jpg", "images/sixGod4.jpg", "images/sixGod5.jpg"]
+}
 
 //Random function
 
@@ -22,6 +32,7 @@ const app = () => {
     //remove existing answer, if it's already on the DOM
 
     $('.answer').remove()
+    $('.image').remove()
 
     //The quiz will compile scores, the caption will come from the winning value.
 
@@ -95,11 +106,13 @@ const app = () => {
 
     //Once you have the grouping, select a random caption from the captions object
 
-    const finalCaption = getRandom(captions[subject]); 
+    const finalCaption = getRandom(captions[subject]);
+
+    const finalImage = getRandom(images[subject])    
     
     $('input:checked').removeAttr('checked');
 
-    $('.questions').after(`<div class="answer"><h2>${finalCaption}</h2><div>`)
+    $('.questions').after(`<img class="image" src="${finalImage}"><div class="answer"><h2>${finalCaption}</h2><div>`)
 
 }
 
